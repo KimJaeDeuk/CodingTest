ed88c6625eb5cee34c553d69413797d5296a70d7
JAEDEUK KIM <kimjaedeuk0@gmail.com>
2023-07-26 18:31:13 +0900

    그래프

--- src/samsung/No16_파핑파핑지뢰찾기.java
+++ src/samsung/No16_파핑파핑지뢰찾기.java
@@ -20,8 +20,10 @@ public class No16_파핑파핑지뢰찾기 {
 	static final int nx[] = {1,1,1,-1,-1,-1,0,0};
 	static final int ny[] = {0,1,-1,0,1,-1,1,-1};
 	static boolean visited[][];
-	static int[][] map;
-	static Queue<Mine> queue = new LinkedList<Mine>();
+	static char[][] map;
+	static int[][] safe;
+	static Queue<Mine> queue;
+	static int N;
 	static int cnt;
 	static int noMineCnt;
 	public static void main(String[] args) throws IOException{
@@ -34,70 +36,109 @@ public class No16_파핑파핑지뢰찾기 {
 		StringBuilder sb = new StringBuilder();
 		String line;
 		for(int tc=1; tc<=test_case; tc++) {
-			int N = Integer.parseInt(br.readLine());
+			N = Integer.parseInt(br.readLine());
 			cnt=0;
 			noMineCnt=0;
-			map = new int[N][N];
+			map = new char[N][N];
+			safe=  new int[N][N];
 			visited = new boolean[N][N];
+			queue = new LinkedList<Mine>();
 			for(int i=0; i<N; i++) {
 				line = br.readLine();
+				map[i] = line.toCharArray();
+			}
+			
+			setMineCnt();
+//			for(int i=0; i<N; i++) {
+//				for(int j=0; j<N; j++) {
+//					System.out.print(safe[i][j]+" ");
+//				}
+//				System.out.println();
+//			}
+			
+			//0인 mine을 눌러보기
+			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
-					if(line.charAt(j)=='.') {
-						map[i][j] =  0 ;
-						noMineCnt++;
-					}else {
-						map[i][j] = -1;
-					}
 					
+					if(safe[i][j]==0 && !visited[i][j]) {
+						bfs(i,j);
+						cnt++;
+					}
+//					}else if(safe[i][j]!=-1 && !visited[i][j]) {
+//						cnt++;
+//					}
 				}
 			}
-			
-			
-			
-			sb.append("#").append(tc);
-			
-			//1차적으로 가장자리가 아니면서 방문하지 않았고 0인경우 bfs를 돌려 확산시키기
-			for(int i=1; i<N-1; i++) {
-				for(int j=1; j<N-1; j++) {
-					if(map[i][j]==0 && !visited[i][j]) {
-						
-						queue.add(new Mine(i,j));
-						visited[i][j] = true;
-						bfs();
-					
-					}
+			for(int i=0; i<N; i++) {
+				for(int j=0;j<N; j++) {
+					if(safe[i][j]!=-1 && !visited[i][j])
+						cnt++;
 				}
 			}
-			//이후에 noMineCnt값만큼 cnt를 더해준다.
+			sb.append("#").append(tc).append(" ");
 			
-			sb.append(cnt+noMineCnt);
+			sb.append(cnt);
+			
 			
 			sb.append("\n");
 		}
 		System.out.println(sb);
 	}
-
-	private static void bfs() {
+	private static void bfs(int i, int j) {
+		queue.add(new Mine(i,j));
+		
+		visited[i][j] = true;
 		
 		while(!queue.isEmpty()) {
 			
 			Mine m = queue.poll();
-			
 			int qx = m.x;
 			int qy = m.y;
-			cnt++;
-			noMineCnt--;
 			
-			for(int k=0; k<8; k++) {
+			int qCnt = 0;
+			
+			for(int k=0;k<8;k++) {
 				
-				int dx = qx+nx[k];
-				int dy = qy+ny[k];
+				int dx = nx[k]+qx;
+				int dy = ny[k]+qy;
 				
+				if(dx<0 || dy<0 || dx>=N || dy>=N || visited[dx][dy]) continue;
+				
+				visited[dx][dy]=true;
+				if(safe[dx][dy]==0) {
+					queue.add(new Mine(dx,dy));
+				}
+			}
+			
+		}
+		
+	}
+	private static void setMineCnt() {
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				int mineCnt = 0;
+				
+				if(map[i][j]=='*') {
+					safe[i][j] = -1;
+					continue;
+				}
+				
+				for(int k=0; k<8; k++) {
+					int dx = nx[k]+i;
+					int dy = ny[k]+j;
+					
+					if(dx<0 || dy<0 || dx>=N || dy>=N) continue;
+					
+					if(map[dx][dy]=='*')
+						mineCnt++;
+				}
+				safe[i][j] = mineCnt;
 				
 			}
 		}
 		
-		
 	}
 
+	
 }
---
+++ src/samsung/No17_영준이의진짜BFS.java
@@ -0,0 +1,79 @@
+package samsung;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class No17_영준이의진짜BFS {
+	static int N;
+	public static void main(String[] args) throws Exception{
+		
+		System.setIn(new FileInputStream("res/17_input.txt"));
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int test_case = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int tc=1; tc<=test_case; tc++) {
+			N = Integer.parseInt(br.readLine());
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+			
+			for(int i=0; i<=N; i++) {
+				list.add(new ArrayList<Integer>());
+			}
+			int tree[][] = new int[N+1][N+1];
+			for(int i=2; i<=N; i++) {
+				int parent = Integer.parseInt(st.nextToken());
+				list.get(parent).add(i);
+				tree[parent][i]=1;
+				tree[i][parent]=1;
+			}
+			
+			sb.append("#").append(tc).append(" ");
+			Queue<Integer> q = new LinkedList<Integer>();
+			q.add(1);
+			int before = -1;
+			int cnt = 0;
+			while(!q.isEmpty()) {
+				int num = q.poll();
+				
+				if(before != -1) {
+					
+					cnt += getMinDist(num, before);
+					
+				}
+				
+				before = num;
+				
+				for(int i=0; i<list.get(num).size(); i++) {
+					q.add(list.get(num).get(i));
+				}
+				
+			}
+			sb.append(cnt);
+			//result
+			
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+	private static int getMinDist(int num1, int num2) {
+		int dist[] = new int[N];
+		int d_i = 1;
+		while(num1!=1) {
+			dist[d_i] = 
+		}
+//		if(level[num1] < level[num2]) {
+//			
+//		}
+//		
+		return 0;
+	}
+
+}
