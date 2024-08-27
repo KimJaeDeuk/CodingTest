d7946681d7802951d7da9b3423dd7be8d80cd2c5
김재득 <93518164+KimJaeDeuk@users.noreply.github.com>
2024-08-27 15:34:31 +0900

    [Gold III] Title: 소수의 연속합, Time: 244 ms, Memory: 27228 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+	static boolean[] sosu = new boolean[4000001];
+	static int cnt=0;
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		if(n==1) {
+			System.out.println(0);
+			return;
+		}
+		//소수만들기
+		for(int i=2; i<sosu.length; i++) {
+			if(!sosu[i]) {
+				make(i);
+			}
+		}
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i=2; i<sosu.length; i++) {
+			if(!sosu[i]) list.add(i); 
+		}
+		
+		
+		for(int i=0; i<list.size(); i++) {
+			
+			int sum = 0;
+			for(int j=i; j<list.size(); j++) {
+				sum += list.get(j);
+				if(sum>n) {
+					break;
+				}else if(sum==n)
+					cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+
+
+	private static void make(int num) {
+		
+		int i=2;
+		while(num*i<sosu.length) {
+			
+			sosu[num*i]=true;
+			i++;
+		}
+		
+	}
+
+
+}
