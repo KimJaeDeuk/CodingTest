9578f887ef72da445b13c6d8d85dfe59683091ea
김재득 <93518164+KimJaeDeuk@users.noreply.github.com>
2024-08-28 14:23:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static StringBuilder sb = new StringBuilder();
+	
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		
+		
+		System.out.println((int)(str.charAt(0)));
+	}
+	
+	
+}
