6bf60a33eb8f34c4e62b8b91287bbd20124c8973
김재득 <93518164+KimJaeDeuk@users.noreply.github.com>
2024-08-28 14:16:05 +0900

    [Bronze V] Title: 고양이, Time: 88 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static StringBuilder sb = new StringBuilder();
+	
+	
+	public static void main(String[] args) throws Exception{
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		System.out.println("\\    /\\\n" +
+							 " )  ( \')\n" +
+							"(  /  )\n" +
+							 " \\(__)|\n");
+		
+	}
+	
+	
+}
